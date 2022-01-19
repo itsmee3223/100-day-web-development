65791aa94b526782d4e250c60033b28d2b0b1d7a
itsmee3223 <tamvam.pemberani@gmail.com>
2022-01-19 23:26:47 +0700

    hashing password with bcryptjs

--- 17-user-authentication-sessions/routes/demo.js
+++ 17-user-authentication-sessions/routes/demo.js
@@ -1,29 +1,46 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.enteredPassword;
 
-router.post('/login', async function (req, res) {});
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
