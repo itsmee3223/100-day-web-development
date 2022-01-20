4cee2a9f9599955fcf0cfae28a7f94e1666419f3
itsmee3223 <tamvam.pemberani@gmail.com>
2022-01-20 14:06:41 +0700

    Add user login and signup validation error message to the website. Authorization user as an admin or not using custom middleware

--- 17-user-authentication-sessions/routes/demo.js
+++ 17-user-authentication-sessions/routes/demo.js
@@ -10,29 +10,64 @@ router.get("/", function (req, res) {
 });
 
 router.get("/signup", function (req, res) {
-  res.render("signup");
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+
+  req.session.inputData = null;
+
+  res.render("signup", { inputData: sessionInputData });
 });
 
 router.get("/login", function (req, res) {
-  res.render("login");
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      password: "",
+    };
+  }
+
+  req.session.inputData = null;
+  res.render("login", { inputData: sessionInputData });
 });
 
 router.post("/signup", async function (req, res) {
   const userData = req.body;
-  const enteredEmail = userData.email;
+  const enteredEmail = userData.email; // userData['email']
   const enteredConfirmEmail = userData["confirm-email"];
   const enteredPassword = userData.password;
 
   if (
     !enteredEmail ||
-    !enteredPassword ||
     !enteredConfirmEmail ||
-    enteredPassword.trim() < 6 ||
+    !enteredPassword ||
+    enteredPassword.trim().length < 6 ||
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes("@")
   ) {
-    console.log("Format data not valid");
-    return res.redirect("/signup");
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check your data.",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+
+    req.session.save(function () {
+      res.redirect("/signup");
+    });
+    return;
+    // return res.render('signup');
   }
 
   const existingUser = await db
@@ -41,8 +76,17 @@ router.post("/signup", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (existingUser) {
-    console.log("User already exist please login or create antoher user");
-    return res.redirect("/signup");
+    req.session.inputData = {
+      hasError: true,
+      message: "User exists already!",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect("/signup");
+    });
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
@@ -68,33 +112,68 @@ router.post("/login", async function (req, res) {
     .findOne({ email: enteredEmail });
 
   if (!existingUser) {
-    console.log("could not login");
-    return res.redirect("/login");
+    req.session.inputData = {
+      hasError: true,
+      message: "Could not log you in - please check your credentials!",
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect("/login");
+    });
+    return;
   }
 
-  const passwordAreEqual = await bcrypt.compare(
+  const passwordsAreEqual = await bcrypt.compare(
     enteredPassword,
     existingUser.password
   );
 
-  if (!passwordAreEqual) {
-    console.log("Password wrong!");
-    return res.redirect("/login");
+  if (!passwordsAreEqual) {
+    req.session.inputData = {
+      hasError: true,
+      message: "Could not log you in - please check your credentials!",
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+    req.session.save(function () {
+      res.redirect("/login");
+    });
+    return;
   }
+
   req.session.user = { id: existingUser._id, email: existingUser.email };
   req.session.isAuthenticated = true;
   req.session.save(function () {
-    res.redirect("/admin");
+    res.redirect("/profile");
   });
 });
 
-router.get("/admin", function (req, res) {
-  if (req.session.isAuthenticated) {// if(!req.session.user)
-    res.status(401).render("401");
+router.get("/admin", async function (req, res) {
+  if (!res.locals.isAuth) {
+    // if (!req.session.user)
+    return res.status(401).render("401");
   }
+
+  if (!res.locals.isAdmin) {
+    return res.status(403).render("403");
+  }
+
   res.render("admin");
 });
 
-router.post("/logout", function (req, res) {});
+router.get("/profile", function (req, res) {
+  if (!res.locals.isAuth) {
+    // if (!req.session.user)
+    return res.status(401).render("401");
+  }
+  res.render("profile");
+});
+
+router.post("/logout", function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
 
 module.exports = router;
