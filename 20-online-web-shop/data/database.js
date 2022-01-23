0fdb96674c67ebd08cfa51591503f21b30dab61d
itsmee3223 <tamvam.pemberani@gmail.com>
2022-01-23 22:12:26 +0700

    fix connection to mongodb

--- 20-online-web-shop/data/database.js
+++ 20-online-web-shop/data/database.js
@@ -1,21 +1,23 @@
 const mongodb = require('mongodb');
 
-const MongoClient = new mongodb.MongoClient
+const MongoClient = mongodb.MongoClient;
+
 let database;
 
-async function connectToDatabse(){
-  const client = await MongoClient.connect('mongodb://localhost:27017')
-  database = client.db('online-shop')
+async function connectToDatabase() {
+  const client = await MongoClient.connect('mongodb://localhost:27017');
+  database = client.db('online-shop');
 }
 
-function getDb(){
-  if(!database){
-    throw new Error('You must connect first')
+function getDb() {
+  if (!database) {
+    throw new Error('You must connect first!');
   }
-  return database
+
+  return database;
 }
 
 module.exports = {
-  connectToDatabse: connectToDatabse,
+  connectToDatabase: connectToDatabase,
   getDb: getDb
-}
+};
