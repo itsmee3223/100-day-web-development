51c39e734d64141ae670e75052ff3b128153c785
itsmee3223 <tamvam.pemberani@gmail.com>
2022-01-25 15:31:45 +0700

    Add product items  into mongoDB

--- 20-online-web-shop/app.js
+++ 20-online-web-shop/app.js
@@ -18,6 +18,7 @@ const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const authRoutes = require("./routes/auth.routes");
 const productsRoutes = require("./routes/products.routes");
 const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
 
 // express use module
 const app = express();
@@ -43,6 +44,7 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use("/admin", adminRoutes);
 
 // error handler
 app.use(errorHandlerMiddleware);
