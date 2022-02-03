0006fc45b28cc956d724493e9412abfba6e88d83
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-03 11:25:41 +0700

    edit button todo function

--- 23-forntend-framework-vue/scripts/app.js
+++ 23-forntend-framework-vue/scripts/app.js
@@ -3,17 +3,38 @@ const TodosApp = {
     return {
       todos: [],
       enteredTodoText: "",
+      editedTodoId: null,
     };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      const newTodo = {
-        text: this.enteredTodoText,
-        id: new Date().toISOString()
+      if (!this.editedTodoId) {
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: new Date().toISOString(),
+        };
+        this.todos.push(newTodo);
+      } else {
+        const todoId = this.editedTodoId;
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return (todoItem.id = todoId);
+        });
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+        this.todos[todoIndex] = updatedTodoItem;
+        this.editedTodoId = null;
       }
-      this.todos.push(newTodo)
-      this.enteredTodoText = ''
+      this.enteredTodoText = "";
+    },
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find(function (todoItem) {
+        return todoItem.id == todoId;
+      });
+      this.enteredTodoText = todo.text;
     },
   },
 };
