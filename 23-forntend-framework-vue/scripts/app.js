250bcb5eb8a82b48ea733a6df884ef1a17bb5aef
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-03 10:52:11 +0700

    Two way binding

--- 23-forntend-framework-vue/scripts/app.js
+++ 23-forntend-framework-vue/scripts/app.js
@@ -1,15 +1,17 @@
 const TodosApp = {
   data() {
     return {
-      newTodo: 'Learn Vue.js'
-    }
+      newTodo: "Learn Vue.js",
+      enteredTodoText: "",
+    };
   },
   methods: {
     saveTodo(event) {
-      event.preventDefault()
-      this.newTodo = 'Updated'
-    }
-  }
-}
+      event.preventDefault();
+      this.newTodo = this.enteredTodoText;
+      this.enteredTodoText = "";
+    },
+  },
+};
 
-Vue.createApp(TodosApp).mount('#todos-app')
+Vue.createApp(TodosApp).mount("#todos-app");
