4b335acd653f27d536e2005c5b989f87d2072df6
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-03 11:48:34 +0700

    send post reques to backend

--- 23-forntend-framework-vue/scripts/app.js
+++ 23-forntend-framework-vue/scripts/app.js
@@ -7,13 +7,38 @@ const TodosApp = {
     };
   },
   methods: {
-    saveTodo(event) {
+    async saveTodo(event) {
       event.preventDefault();
       if (!this.editedTodoId) {
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+
         const newTodo = {
           text: this.enteredTodoText,
-          id: new Date().toISOString(),
+          id: responseData.createdTodo.id,
         };
+
         this.todos.push(newTodo);
       } else {
         const todoId = this.editedTodoId;
