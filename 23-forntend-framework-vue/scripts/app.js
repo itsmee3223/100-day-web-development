b303448c66157e5c2e4376966b764e46c97798b5
itsmee3223 <tamvam.pemberani@gmail.com>
2022-02-04 09:40:55 +0700

    update and delete request to backend api

--- 23-forntend-framework-vue/scripts/app.js
+++ 23-forntend-framework-vue/scripts/app.js
@@ -1,6 +1,7 @@
 const TodosApp = {
   data() {
     return {
+      isLoading: false,
       todos: [],
       enteredTodoText: "",
       editedTodoId: null,
@@ -9,7 +10,46 @@ const TodosApp = {
   methods: {
     async saveTodo(event) {
       event.preventDefault();
-      if (!this.editedTodoId) {
+
+      if (this.editedTodoId) {
+        // Updating...
+        const todoId = this.editedTodoId;
+
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return todoItem.id === todoId;
+        });
+
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+
+        this.todos[todoIndex] = updatedTodoItem;
+        this.editedTodoId = null;
+
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos/" + todoId, {
+            method: "PATCH",
+            body: JSON.stringify({
+              newText: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+      } else {
+        // Creating...
         let response;
 
         try {
@@ -40,32 +80,59 @@ const TodosApp = {
         };
 
         this.todos.push(newTodo);
-      } else {
-        const todoId = this.editedTodoId;
-        const todoIndex = this.todos.findIndex(function (todoItem) {
-          return (todoItem.id = todoId);
-        });
-        const updatedTodoItem = {
-          id: this.todos[todoIndex].id,
-          text: this.enteredTodoText,
-        };
-        this.todos[todoIndex] = updatedTodoItem;
-        this.editedTodoId = null;
       }
+
       this.enteredTodoText = "";
     },
     startEditTodo(todoId) {
       this.editedTodoId = todoId;
       const todo = this.todos.find(function (todoItem) {
-        return todoItem.id == todoId;
+        return todoItem.id === todoId;
       });
       this.enteredTodoText = todo.text;
     },
-    deleteTodo(todoId) {
+    async deleteTodo(todoId) {
       this.todos = this.todos.filter(function (todoItem) {
         return todoItem.id !== todoId;
       });
+
+      let response;
+
+      try {
+        response = await fetch("http://localhost:3000/todos/" + todoId, {
+          method: "DELETE",
+        });
+      } catch (error) {
+        alert("Something went wrong!");
+        return;
+      }
+
+      if (!response.ok) {
+        alert("Something went wrong!");
+        return;
+      }
     },
+  },
+  async created() {
+    let response;
+    this.isLoading = true;
+    try {
+      response = await fetch("http://localhost:3000/todos");
+    } catch (error) {
+      alert("Something went wrong!");
+      this.isLoading = false;
+      return;
+    }
+
+    this.isLoading = false;
+
+    if (!response.ok) {
+      alert("Something went wrong!");
+      return;
+    }
+
+    const responseData = await response.json();
+    this.todos = responseData.todos;
   },
 };
 
